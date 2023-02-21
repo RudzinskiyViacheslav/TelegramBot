c77c2ec5f35cd2649802d4bec4b34265df917ba8
Rudzinsky Viacheslav <slava_99@list.ru>
2023-02-21 03:38:19 +0300

    Бот тепеь переделывает фото только при наличии фразы. Возможность узнать в реальном времени цены на алко с сайта едадил

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 from random import choice
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont, ImageColor, ImageOps
 #from token import *
 from parser import *
 
@@ -9,7 +9,7 @@ token = '6056207304:AAE_p4qJMQYAv1mMrXwuZe95naZcbwmd4z8'
 bot = telebot.TeleBot(token)
 drinks = ['Водка', 'Виски', 'Джин', 'Коньяк', 'Текила', 'Пиво', 'Ничего']
 url_github_egor = 'https://github.com/eeroshkin/bot'
-url_winelab_vodka = 'https://www.winelab.ru/catalog/krepkiy-alkogol-vodka/cp_0.5?sort=price-asc'
+url_edadil_winelab = 'https://edadeal.ru/moskva/offers?q=%D0%B2%D0%BE%D0%B4%D0%BA%D0%B0%200%2C5%20%D0%BB&retailer=winelab&sort=aprice'
 
 def what_to_drink():
     drink = choice(drinks)
@@ -43,7 +43,7 @@ def start_message(message):
     item5 = types.KeyboardButton('Отправь фото')
     item6 = types.KeyboardButton('Покажи цены на водку')
 
-    markup_for_group.add(item1, item2, item3, item4, item6)
+    markup_for_group.add(item1, item2, item4, item6)
 
     markup_for_chat.add(item1, item2, item3, item5, item6)
 
@@ -51,6 +51,7 @@ def start_message(message):
         bot.send_message(message.chat.id, f'Приветик {message.from_user.username}', reply_markup=markup_for_chat)
     elif message.chat.type == 'group' or message.chat.type == 'supergroup':
         bot.send_message(message.chat.id, f'Приветик {message.from_user.username}', reply_markup=markup_for_group)
+
     # print(f"{message.from_user.username} пишет {message.text}")
     # if message.text == "Привет":
     # print(f"{message.from_user.username} пишет {message.text}")
@@ -60,7 +61,7 @@ def start_message(message):
 
 
 @bot.message_handler(content_types=['text'])
-def message_reply(message):
+def handle_photo_and_text(message):
     if message.text == "Создатель бота":
         bot.send_message(message.chat.id, "@i_am_monarch")
     elif message.text == "Что выпить":
@@ -76,19 +77,38 @@ def message_reply(message):
     elif message.text == 'Отправь фото':
         photo = open('wallpaper.jpg', 'rb')
         bot.send_photo(message.chat.id, photo)
+    elif message.text == 'Покажи цены на водку':
+        str_AGE_CHECK, str_vodkas = get_vodka_price_winelab(url_edadil_winelab)
+        bot.send_message(message.chat.id, f'{str_AGE_CHECK}\nИз ВинЛаба:\n{str_vodkas}')
 
 
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
-    photo = message.photo[-1]
-    file_info = bot.get_file(photo.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    with open("test_photo.jpg", 'wb') as file:
-        file.write(downloaded_file)
-    photo_to_change = Image.open("test_photo.jpg")
-    changed_photo = photo_to_change.convert("L")
+    if message.caption == 'Сделай':
+        photo = message.photo[-1]
+        file_info = bot.get_file(photo.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        with open("test_photo.jpg", 'wb') as file:
+            file.write(downloaded_file)
+        photo_to_change = Image.open("test_photo.jpg")
+        changed_photo = ImageOps.grayscale(photo_to_change)
 
-    bot.send_photo(message.chat.id, changed_photo)
+        draw = ImageDraw.Draw(changed_photo)
+        font = ImageFont.truetype('Verdana.ttf', 99)
+        text = 'Будь здоров'
+        color = ImageColor.getrgb("red")
+
+        image_width, image_height = changed_photo.size
+        (text_width, text_height, *_)=draw.textbbox((0, 0), text, font=font)
+        #text_width, text_height = draw.textsize(text, font)
+
+        center_x = (image_width - text_width) / 14
+        center_y = (image_height - text_height) / 2
+
+        draw.text((center_x, center_y), text, font=font, fill="#FF0000")
+
+        bot.send_photo(message.chat.id, changed_photo)
+
 
 
 bot.infinity_polling()
--- parser.py
+++ parser.py
@@ -1,6 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 #import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from fake_useragent import UserAgent
+import time
 
 def get_github_data(url):
     response = requests.get(url)
@@ -31,20 +38,75 @@ def get_github_data(url):
     #print(item.text.strip())
 
 def get_vodka_price_winelab(url):
-    response = requests.get(url)
-    content = response.text
-    soup = BeautifulSoup(content, 'html.parser')
+    ua = UserAgent()
+    user_agent = ua.chrome
+    AGE_CHECK = True
 
-    soup_find = soup.find_all('div', class_=' col-12 col-sm-6 col-md-6 col-lg-4')
+    options = Options()
+    options.add_argument('--window-size=1400,1200')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument("--disable-extensions")
+    options.add_argument('--remote-debugging-port=9222')
+    options.add_argument('--enable-javascript')
+    options.add_argument('--no-sandbox')
+    options.add_argument("--allow-running-insecure-content")
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--allow-insecure-localhost')
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; "
+                         "x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'")
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
 
-    if response.status_code:
-        print(f'Подключение в норме и установлено с кодом {response.status_code}')
+    chrome_driver = webdriver.Chrome(options=options)
+    # chrome_driver.delete_cookie()
+    # chrome_driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+    time.sleep(5)
 
-    vodka_from_winelab_list =[]
-    for item in soup_find:
-        vodka_from_winelab_list.append(item.text.strip())
+    chrome_driver.get(url)
+    time.sleep(5)
 
-    vodka_from_winelab_str = 0
+    try:
+        age_confirm_btn = chrome_driver.find_element(By.XPATH,
+                                                     "//div[@class='b-button b-button_disabled_false b-button_theme_default b-button_shape_default b-button_size_m b-button_justify_start b-button_selected_false b-offer__alco-action']")
+        age_confirm_btn.click()
+        #print(age_confirm_btn)
+    except Exception as exp:
+        AGE_CHECK = False
 
+    vodka_names = chrome_driver.find_elements(By.XPATH, '//div[@class="b-offer__description"]')
 
+    # try:
+    #     vodka_names = chrome_driver.find_element(By.CLASS_NAME, "p-offers__header")
+    #     print(vodka_names)
+    # except Exception as exc:
+    #     print(exc)
+
+    vodka_prices_sale = chrome_driver.find_elements(By.XPATH, '//div[@class="b-offer__price-new"]')
+    # print(vodka_prices_sale)
+
+    vodka_prices_normal = chrome_driver.find_elements(By.XPATH, '//div[@class="b-offer__price-old"]')
+    # print(vodka_prices_normal)
+
+    list_vodkas_to_send_Bot = []
+
+    for i in range(len(vodka_names)):
+        list_vodkas_to_send_Bot.append(f'{vodka_names[i].text} нынче по цене {vodka_prices_sale[i].text}, '
+                                       f'а была {vodka_prices_normal[i].text}\n')
+
+    str_vodkas_to_send_Bot = '\n'.join(list_vodkas_to_send_Bot)
+    # for item in list_vodkas_to_send_Bot:
+    #     print(item)
+
+    # for item in vodka_names:
+    #     print(item.text)
+
+    chrome_driver.quit()
+    if AGE_CHECK:
+        str_AGE_CHECK = 'Была проверка на возраст, но я ее обошел!\n'
+    else:
+        str_AGE_CHECK = 'Даже не было проверки на возраст)\n'
+
+    return str_AGE_CHECK, str_vodkas_to_send_Bot
 
